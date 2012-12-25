3aed12da56a908f630c0552863d89580d3334a35
77web <info@77-web.com>
2012-12-25 22:15:28 +0900

    日本の都道府県のchoice:PrejectureTypeを追加

---
+++ Tests/Type/PrefectureTypeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Nanaweb\JapaneseFormBundle\Tests\Type;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PrefectureTypeTest extends WebTestCase
+{
+    public function test()
+    {
+        $client = static::createClient();
+        
+        $container = $client->getContainer();
+        
+        $form = $container
+                    ->get('form.factory')
+                    ->createBuilder()
+                    ->add('pref', 'jp_prefecture')
+                    ->getForm()
+        ;
+        $formView = $form->createView();
+        
+        $this->assertEquals(47, count($formView['pref']->vars['choices']));
+    }
+}
---
+++ Type/PrefectureType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Nanaweb\JapaneseFormBundle\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PrefectureType extends AbstractType
+{
+    private $prefectures;
+    
+    public function __construct(array $prefectures)
+    {
+        $this->prefectures = $prefectures;
+    }
+    
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'choices' => $this->prefectures,
+        ));
+    }
+    
+    public function getParent()
+    {
+        return 'choice';
+    }
+    
+    public function getName()
+    {
+        return 'jp_prefecture';
+    }
+}
