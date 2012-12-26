26d42b66d936d1dc2eaf9ca26d66b8ff155650f8
77web <info@77-web.com>
2012-12-26 09:23:08 +0900

    services.ymlの記述のテストを追加

--- Tests/Form/Type/PrefectureTypeTest.php
+++ Tests/Form/Type/PrefectureTypeTest.php
@@ -3,6 +3,7 @@
 namespace Nanaweb\JapaneseFormBundle\Tests\Form\Type;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Form\FormException;
 
 class PrefectureTypeTest extends WebTestCase
 {
@@ -12,12 +13,20 @@ class PrefectureTypeTest extends WebTestCase
         
         $container = $client->getContainer();
         
-        $form = $container
-                    ->get('form.factory')
-                    ->createBuilder()
-                    ->add('pref', 'jp_prefecture')
-                    ->getForm()
-        ;
+        try
+        {
+            $form = $container
+                        ->get('form.factory')
+                        ->createBuilder()
+                        ->add('pref', 'jp_prefecture')
+                        ->getForm()
+            ;
+            $this->assertTrue(true);
+        }
+        catch (FormException $e)
+        {
+            $this->fail($e->getMessage());
+        }
         $formView = $form->createView();
         
         $this->assertEquals(47, count($formView['pref']->vars['choices']));
